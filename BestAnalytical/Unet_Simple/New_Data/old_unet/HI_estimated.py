e07780d28d53617ac278f8f2c4513b54f8835ca0
HelenShao <52927975+HelenShao@users.noreply.github.com>
2020-07-25 14:59:39 -0400

    Update HI_estimated.py

--- BestAnalytical/Unet_Simple/New_Data/old_unet/HI_estimated.py
+++ BestAnalytical/Unet_Simple/New_Data/old_unet/HI_estimated.py
@@ -5,14 +5,17 @@ import matplotlib.pyplot as plt
 DM= np.load('DM_maps_new.npy')
 HI= np.load('HI_maps_new.npy')
 
+DM = np.log10(DM+1)
+HI = np.log10(HI+1)
+
 # Create Histogram
-bins_DM = np.linspace(0,28,50)      #0/28 are the min/max values of the DM maps
-bins_DM_mean = 0.5*(bins_DM[1:] + bins_DM[:-1] )     
+bins_DM = np.linspace(np.min(DM),np.max(DM),50)      #min/max values of the DM maps
+bins_DM_mean = 0.5*(bins_DM[1:] + bins_DM[:-1] )
 Number_points = np.histogram(DM, bins=bins_DM)[0]
 HI_total      = np.histogram(DM, bins=bins_DM, weights=HI)[0]
 HI_mean = HI_total / Number_points
 HI_mean = np.nan_to_num(HI_mean)    #Change NaN values in array to 0
-    
-# Estimate HI using the best fit line and load into HI_estimation matrix 
+
+# Estimate HI using the best fit line and load into HI_estimation matrix
 HI_estimation = np.interp(DM, bins_DM_mean, HI_mean)
 np.save('HI_estimated_large.npy', HI_estimation)
